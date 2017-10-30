a9130bfa2ffa892bce14d2a32dcbd7fe5dfc2db5
VeggieDealer <31700978+VeggieDealer@users.noreply.github.com>
2017-10-30 10:45:20 -0600

    and leave tear drops everywhere

--- functions.js
+++ functions.js
@@ -1,113 +1,131 @@
 function drawMap() {
-  strokeWeight(10);
-  stroke(0);
-  line(0, 100, 1375, 100);
-  line(1375, 100, 1375, 1200);
-  line(1375, 1200, 100, 1200);
-  line(100, 1200, 100, 200);
-  line(100, 200, 1250, 200);
-  line(1250, 200, 1250, 1100);
-  line(1250, 1100, 200, 1100);
-  line(200, 1100, 200, 300);
-  line(200, 300, 1150, 300);
-  line(1150, 300, 1150, 1025);
-  line(1150, 1025, 300, 1025);
-  line(300, 1025, 300, 375);
-  line(300, 375, 1050, 375);
-  line(1050, 375, 1050, 950);
-  line(1050, 950, 400, 950);
-  line(400, 950, 400, 475);
-  line(400, 475, 950, 475);
-  line(950, 475, 950, 850);
-  line(950, 850, 500, 850);
-  line(500, 850, 500, 550);
-  line(500, 550, 850, 550);
-  line(850, 550, 850, 775);
-  line(850, 775, 600, 775);
-  line(600, 775, 600, 650)
+    strokeWeight(10);
+    stroke(0);
+    line(0, 100, 1375, 100);
+    line(1375, 100, 1375, 1200);
+    line(1375, 1200, 100, 1200);
+    line(100, 1200, 100, 200);
+    line(100, 200, 1250, 200);
+    line(1250, 200, 1250, 1100);
+    line(1250, 1100, 200, 1100);
+    line(200, 1100, 200, 300);
+    line(200, 300, 1150, 300);
+    line(1150, 300, 1150, 1025);
+    line(1150, 1025, 300, 1025);
+    line(300, 1025, 300, 375);
+    line(300, 375, 1050, 375);
+    line(1050, 375, 1050, 950);
+    line(1050, 950, 400, 950);
+    line(400, 950, 400, 475);
+    line(400, 475, 950, 475);
+    line(950, 475, 950, 850);
+    line(950, 850, 500, 850);
+    line(500, 850, 500, 550);
+    line(500, 550, 850, 550);
+    line(850, 550, 850, 775);
+    line(850, 775, 600, 775);
+    line(600, 775, 600, 650)
 }
 
 function drawCharacter() {
-  stroke("blue")
-  strokeWeight(1);
-  fill("blue");
-  ellipse(runling.position.x, runling.position.y, 4);
+    stroke("blue")
+    strokeWeight(1);
+    fill("blue");
+    ellipse(runling.position.x, runling.position.y, 4);
 
-  // runling speeds
+    // runling speeds
 
 
 
-  if (runling.speed < 0.8 && lane == 2) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 1 && lane == 3) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 1.2 && lane == 4) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 1.4 && lane == 5) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 1.6 && lane == 6) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 1.8 && lane == 7) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 2 && lane == 8) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 2.2 && lane == 9) {
-    runling.speed += 0.2;
-  } else if (runling.speed < 2.4 && lane == 10) {
-    runling.speed += 0.2;
-  }
+    if (runling.speed < 0.8 && lane == 2) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 1 && lane == 3) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 1.2 && lane == 4) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 1.4 && lane == 5) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 1.6 && lane == 6) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 1.8 && lane == 7) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 2 && lane == 8) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 2.2 && lane == 9) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 2.4 && lane == 10) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 2.6 && lane == 11) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 2.8 && lane == 12) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 3 && lane == 13) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 3.2 && lane == 14) {
+        runling.speed += 0.2;
+    } else if (runling.speed < 3.4 && lane == 15) {
+        runling.speed += 0.2;
+    }
 }
 
 function boundaries() {
-  //checking which lane the character is in
+    //checking which lane the character is in
 
-  if (runling.position.x > 0 && runling.position.x < 1375 && runling.position.y > 0 && runling.position.y < 100) {
-    lane = 1;
-  } else if (runling.position.x > 1375 && runling.position.x < width && runling.position.y > 0 && runling.position.y < 1205) {
-    lane = 2;
-  } else if (runling.position.x < width && runling.position.x > 95 && runling.position.y > 1200 && runling.position.y < height) {
-    lane = 3;
-  } else if (runling.position.x < 100 && runling.position.x > 0 && runling.position.y > 200 && runling.position.y < height) {
-    lane = 4;
-  } else if (runling.position.x > 0 && runling.position.x < 1245 && runling.position.y > 110 && runling.position.y < 200) {
-    lane = 5;
-  } else if (runling.position.x > 1250 && runling.position.x < 1375 && runling.position.y > 110 && runling.position.y < 1100) {
-    lane = 6;
-  } else if (runling.position.x > 200 && runling.position.x < 1375 && runling.position.y < 1190 && runling.position.y > 1105) {
-    lane = 7;
-  } else if (runling.position.x > 100 && runling.position.x < 210 && runling.position.y > 310 && runling.position.y < 1190) {
-    lane = 8;
-  } else if (runling.position.x > 100 && runling.position.x < 1240 && runling.position.y > 200 && runling.position.y < 300) {
-    lane = 9;
-  }
+    if (runling.position.x > 0 && runling.position.x < 1375 && runling.position.y > 0 && runling.position.y < 100) {
+        lane = 1;
+    } else if (runling.position.x > 1375 && runling.position.x < width && runling.position.y > 0 && runling.position.y < 1205) {
+        lane = 2;
+    } else if (runling.position.x < width && runling.position.x > 95 && runling.position.y > 1200 && runling.position.y < height) {
+        lane = 3;
+    } else if (runling.position.x < 100 && runling.position.x > 0 && runling.position.y > 200 && runling.position.y < height) {
+        lane = 4;
+    } else if (runling.position.x > 0 && runling.position.x < 1245 && runling.position.y > 110 && runling.position.y < 200) {
+        lane = 5;
+    } else if (runling.position.x > 1250 && runling.position.x < 1375 && runling.position.y > 110 && runling.position.y < 1100) {
+        lane = 6;
+    } else if (runling.position.x > 200 && runling.position.x < 1375 && runling.position.y < 1190 && runling.position.y > 1105) {
+        lane = 7;
+    } else if (runling.position.x > 100 && runling.position.x < 210 && runling.position.y > 310 && runling.position.y < 1190) {
+        lane = 8;
+    } else if (runling.position.x > 100 && runling.position.x < 1140 && runling.position.y > 200 && runling.position.y < 300) {
+        lane = 9;
+    } else if (runling.position.x > 1150 && runling.position.x < 1245 && runling.position.y > 200 && runling.position.y < 1100) {
+        lane = 10;
+    } else if (runling.position.x > 210 && runling.position.x < 1245 && runling.position.y > 1030 && runling.position.y < 1095) {
+        lane = 11;
+    } else if (runling.position.x > 210 && runling.position.x < 290 && runling.position.y > 300 && runling.position.y < 1095) {
+        lane = 12;
+    } else if (runling.position.x > 210 && runling.position.x < 1150 && runling.position.y > 300 && runling.position.y < 390) {
+        lane = 13;
+    }
 
-  if (lane == 1) {
-    runling.position.x = constrain(runling.position.x, 3, width - 3);
-    runling.position.y = constrain(runling.position.y, 3, 93);
-  } else if (lane == 2) {
-    runling.position.x = constrain(runling.position.x, 1385, width - 3);
-    runling.position.y = constrain(runling.position.y, 3, height - 3);
-  } else if (lane == 3) {
-    runling.position.x = constrain(runling.position.x, 0, width - 3);
-    runling.position.y = constrain(runling.position.y, 1210, height - 3);
-  } else if (lane == 4) {
-    runling.position.x = constrain(runling.position.x, 0, 90);
-    runling.position.y = constrain(runling.position.y, 100, height - 3);
-  } else if (lane == 5) {
-    runling.position.x = constrain(runling.position.x, 3, 1365);
-    runling.position.y = constrain(runling.position.y, 110, 193);
-  } else if (lane == 6) {
-    runling.position.x = constrain(runling.position.x, 1258, 1365);
-    runling.position.y = constrain(runling.position.y, 110, 1190);
-  } else if (lane == 7) {
-    runling.position.x = constrain(runling.position.x, 110, 1365);
-    runling.position.y = constrain(runling.position.y, 1110, 1190);
-  } else if (lane == 8) {
-    runling.position.x = constrain(runling.position.x, 110, 190);
-    runling.position.y = constrain(runling.position.y, 210, 1190);
-  } else if (lane == 9) {
-    runling.position.x = constrain(runling.position.x, 110, 1240);
-    runling.position.y = constrain(runling.position.y, 210, 290);
-  }
-    
+    if (lane == 1) {
+        runling.position.x = constrain(runling.position.x, 3, width - 3);
+        runling.position.y = constrain(runling.position.y, 3, 93);
+    } else if (lane == 2) {
+        runling.position.x = constrain(runling.position.x, 1385, width - 3);
+        runling.position.y = constrain(runling.position.y, 3, height - 3);
+    } else if (lane == 3) {
+        runling.position.x = constrain(runling.position.x, 0, width - 3);
+        runling.position.y = constrain(runling.position.y, 1210, height - 3);
+    } else if (lane == 4) {
+        runling.position.x = constrain(runling.position.x, 0, 90);
+        runling.position.y = constrain(runling.position.y, 100, height - 3);
+    } else if (lane == 5) {
+        runling.position.x = constrain(runling.position.x, 3, 1365);
+        runling.position.y = constrain(runling.position.y, 110, 193);
+    } else if (lane == 6) {
+        runling.position.x = constrain(runling.position.x, 1258, 1365);
+        runling.position.y = constrain(runling.position.y, 110, 1190);
+    } else if (lane == 7) {
+        runling.position.x = constrain(runling.position.x, 110, 1365);
+        runling.position.y = constrain(runling.position.y, 1110, 1190);
+    } else if (lane == 8) {
+        runling.position.x = constrain(runling.position.x, 110, 190);
+        runling.position.y = constrain(runling.position.y, 210, 1190);
+    } else if (lane == 9) {
+        runling.position.x = constrain(runling.position.x, 110, 1240);
+        runling.position.y = constrain(runling.position.y, 210, 290);
+    }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,15 +1,15 @@
 let runlingDestination;
 let runling = {
-  position: 0,
-  speed: 0.7
+    position: 0,
+    speed: 0.7
 }
 let runlingMove = false;
 let moveVector = false;
 drones = [];
 let baseDrone = {
-  position: 0,
-  r: 15,
-  move: 0,
+    position: 0,
+    r: 15,
+    move: 0,
 }
 let lane = 0;
 let speedComplete = false;
@@ -19,81 +19,81 @@ let godMode = -1;
 
 // SETUP FUNCTION - Runs once at beginning of program
 function setup() {
-  createCanvas(1500, 1300);
-  runling.position = createVector(10, 10);
-  runlingDestination = createVector(0, 0);
-  droneNumber = 0;
-  baseDrone.move = p5.Vector.random2D(-1,1);
-  baseDrone.position = createVector(0,0);
-  //Calling localStorage
-  runling.speed = parseFloat(localStorage.getItem("speed"));    
+    createCanvas(1500, 1300);
+    runling.position = createVector(10, 10);
+    runlingDestination = createVector(0, 0);
+    droneNumber = 0;
+    baseDrone.move = p5.Vector.random2D(-1, 1);
+    baseDrone.position = createVector(0, 0);
+    //Calling localStorage
+    runling.speed = parseFloat(localStorage.getItem("speed"));
 
 }
 
 // DRAW FUNCTION - Loops @ 60FPS by default
 function draw() {
-  background(200, 200, 200);
-  fill(200, 200, 200);
+    background(200, 200, 200);
+    fill(200, 200, 200);
 
-  if (isNaN(runling.speed)) {
-      console.log("NONONONONONO");
-      runling.speed = 0.7;
-  }
-  fill("white");
-  //safe zone 1
-  rect(0, 0, 100, 100);
-  //safe zone 2
-  rect(1377, 0, 122, 96);
-  //safe zone 3
-  rect(1377, 1200, 120, 99);
-  //safe zone 4
-  rect(0, 1200, 100, 99);
-  drawMap();
-  drawCharacter();
-  boundaries();
+    if (isNaN(runling.speed)) {
+        console.log("NONONONONONO");
+        runling.speed = 0.7;
+    }
+    fill("white");
+    //safe zone 1
+    rect(0, 0, 100, 100);
+    //safe zone 2
+    rect(1377, 0, 122, 96);
+    //safe zone 3
+    rect(1377, 1200, 120, 99);
+    //safe zone 4
+    rect(0, 1200, 100, 99);
+    drawMap();
+    drawCharacter();
+    boundaries();
 
-  stroke(0);
-
-
-  // Assigning the drones random spots within the lane and giving them random speeds
-  //lane 1 drone draw
-  if (droneNumber < 11) {
-    drones.push(Object.assign({}, baseDrone));
-    drones[drones.length - 1].position = createVector();
-    drones[drones.length - 1].position.x = random(100, 1300);
-    drones[drones.length - 1].position.y = random(10, 95);
-    drones[drones.length - 1].move = p5.Vector.random2D();
-    drones[drones.length - 1].move.setMag(1.5);
+    stroke(0);
 
 
-    droneNumber++;
-
-    //Drawing Drones in lane 2
-  }
-
-
-
-  if (droneNumber < 11) {
-    drones.push(Object.assign({}, baseDrone));
-    drones[drones.length - 1].position = createVector();
-    drones[drones.length - 1].position.x = random(1380, 1490);
-    drones[drones.length - 1].position.y = random(110, 1100);
-    drones[drones.length - 1].move = p5.Vector.random2D();
-    drones[drones.length - 1].move.setMag(1.5);
+    // Assigning the drones random spots within the lane and giving them random speeds
+    //lane 1 drone draw
+    if (droneNumber < 11) {
+        drones.push(Object.assign({}, baseDrone));
+        drones[drones.length - 1].position = createVector();
+        drones[drones.length - 1].position.x = random(100, 1300);
+        drones[drones.length - 1].position.y = random(10, 88);
+        drones[drones.length - 1].move = p5.Vector.random2D();
+        drones[drones.length - 1].move.setMag(1.5);
 
 
-  }
-  //Drawing drones in lane 3
-  if (droneNumber < 11) {
-    drones.push(Object.assign({}, baseDrone));
-    drones[drones.length - 1].position = createVector();
-    drones[drones.length - 1].position.x = random(160, 1300);
-    drones[drones.length - 1].position.y = random(1220, height - 11);
-    drones[drones.length - 1].move = p5.Vector.random2D();
-    drones[drones.length - 1].move.setMag(1.5);
-  }
-    
-  //Drawing drones in lane 4
+        droneNumber++;
+
+        //Drawing Drones in lane 2
+    }
+
+
+
+    if (droneNumber < 11) {
+        drones.push(Object.assign({}, baseDrone));
+        drones[drones.length - 1].position = createVector();
+        drones[drones.length - 1].position.x = random(1380, 1490);
+        drones[drones.length - 1].position.y = random(110, 1100);
+        drones[drones.length - 1].move = p5.Vector.random2D();
+        drones[drones.length - 1].move.setMag(1.5);
+
+
+    }
+    //Drawing drones in lane 3
+    if (droneNumber < 11) {
+        drones.push(Object.assign({}, baseDrone));
+        drones[drones.length - 1].position = createVector();
+        drones[drones.length - 1].position.x = random(160, 1300);
+        drones[drones.length - 1].position.y = random(1220, height - 11);
+        drones[drones.length - 1].move = p5.Vector.random2D();
+        drones[drones.length - 1].move.setMag(1.5);
+    }
+
+    //Drawing drones in lane 4
     if (droneNumber < 11) {
         drones.push(Object.assign({}, baseDrone));
         drones[drones.length - 1].position = createVector();
@@ -102,98 +102,98 @@ function draw() {
         drones[drones.length - 1].move = p5.Vector.random2D();
         drones[drones.length - 1].move.setMag(1.5);
     }
-    
+
     //
 
-  //
+    //
 
-  // Creating the drones
-  for (i = 0; i < drones.length; i++) {
-    fill("black");
-    ellipse(drones[i].position.x, drones[i].position.y, drones[i].r);
-    drones[i].position.add(drones[i].move);
-    //lane one drones rebounce
-    if (drones[i].position.x < 107 && drones[i].position.x > 100 && drones[i].position.y > 10 && drones[i].position.y < 97) {
-      drones[i].move.x *= -1;
-    } else if (drones[i].position.x > 1370 && drones[i].position.x < 1375 && drones[i].position.y > 10 && drones[i].position.y < 97) {
-      drones[i].move.x *= -1;
+    // Creating the drones
+    for (i = 0; i < drones.length; i++) {
+        fill("black");
+        ellipse(drones[i].position.x, drones[i].position.y, drones[i].r);
+        drones[i].position.add(drones[i].move);
+        //lane one drones rebounce
+        if (drones[i].position.x < 107 && drones[i].position.x > 100 && drones[i].position.y > 10 && drones[i].position.y < 88) {
+            drones[i].move.x *= -1;
+        } else if (drones[i].position.x > 1370 && drones[i].position.x < 1375 && drones[i].position.y > 10 && drones[i].position.y < 88) {
+            drones[i].move.x *= -1;
+        }
+        if (drones[i].position.y < 10) {
+            drones[i].move.y *= -1;
+        } else if (drones[i].position.y > 88 && drones[i].position.y < 100 && drones[i].position.x > 107) {
+            drones[i].move.y *= -1;
+        } else if (drones[i].position.y > 88 && drones[i].position.y < 100 && drones[i].position.x < 1370) {
+            drones[i].move.y *= -1;
+        }
+
+
+        //lane two Drones rebounce
+        if (drones[i].position.x < 1385 && drones[i].position.x > 1375 && drones[i].position.y > 100) {
+            drones[i].move.x *= -1;
+        } else if (drones[i].position.x > width - 10 && drones[i].position.y > 100) {
+            drones[i].move.x *= -1;
+        }
+        if (drones[i].position.y < 105 && drones[i].position.x < width && drones[i].position.x > 1380) {
+            drones[i].move.y *= -1;
+        } else if (drones[i].position.y > 1192 && drones[i].position.x < width && drones[i].position.x > 1380) {
+            drones[i].move.y *= -1;
+        }
+
+        //lane three Drones rebounce
+
+        if (drones[i].position.x < 110 && drones[i].position.y > 1200) {
+            drones[i].move.x *= -1;
+        } else if (drones[i].position.x > 1370 && drones[i].position.y > 1200) {
+            drones[i].move.x *= -1;
+        }
+        if (drones[i].position.y < 1212 && drones[i].position.y > 1210 && drones[i].position.x > 100 && drones[i].position.x < 1380) {
+            drones[i].move.y *= -1;
+        } else if (drones[i].position.y > height - 10) {
+            drones[i].move.y *= -1;
+        }
+
+
+        //Drone Collision
+        if (dist(runling.position.x, runling.position.y, drones[i].position.x, drones[i].position.y) < drones[i].r - 3 && godMode == -1) {
+            runling.position.x = 10;
+            runling.position.y = 10;
+            runlingMove = false;
+        }
     }
-    if (drones[i].position.y < 10) {
-      drones[i].move.y *= -1;
-    } else if (drones[i].position.y > 97 && drones[i].position.y < 100 && drones[i].position.x > 107) {
-      drones[i].move.y *= -1;
-    } else if (drones[i].position.y > 97 && drones[i].position.y < 100 && drones[i].position.x < 1370) {
-      drones[i].move.y *= -1;
+
+
+    // Moving the character
+    if (runlingMove) {
+        moveVector = p5.Vector.sub(runlingDestination, runling.position);
+        if (moveVector.mag() > runling.speed) {
+            moveVector.div(moveVector.mag() / runling.speed);
+            runling.position.add(moveVector);
+        }
     }
 
-
-    //lane two Drones rebounce
-    if (drones[i].position.x < 1380 && drones[i].position.x > 1375 && drones[i].position.y > 100) {
-      drones[i].move.x *= -1;
-    } else if (drones[i].position.x > width && drones[i].position.y > 100) {
-      drones[i].move.x *= -1;
-    }
-    if (drones[i].position.y < 105 && drones[i].position.x < width && drones[i].position.x > 1380) {
-      drones[i].move.y *= -1;
-    } else if (drones[i].position.y > 1192 && drones[i].position.x < width && drones[i].position.x > 1380) {
-      drones[i].move.y *= -1;
-    }
-
-    //lane three Drones rebounce
-
-    if (drones[i].position.x < 110 && drones[i].position.y > 1200) {
-      drones[i].move.x *= -1;
-    } else if (drones[i].position.x > 1370 && drones[i].position.y > 1200) {
-      drones[i].move.x *= -1;
-    }
-    if (drones[i].position.y < 1212 && drones[i].position.y > 1210 && drones[i].position.x > 100 && drones[i].position.x < 1380) {
-      drones[i].move.y *= -1;
-    } else if (drones[i].position.y > height) {
-      drones[i].move.y *= -1;
-    }
-
-
-    //Drone Collision
-    if (dist(runling.position.x, runling.position.y, drones[i].position.x, drones[i].position.y) < drones[i].r - 3 && godMode == -1) {
-      runling.position.x = 10;
-      runling.position.y = 10;
-      runlingMove = false;
-    }
-  }
-
-
-  // Moving the character
-  if (runlingMove) {
-    moveVector = p5.Vector.sub(runlingDestination, runling.position);
-    if (moveVector.mag() > runling.speed) {
-      moveVector.div(moveVector.mag() / runling.speed);
-      runling.position.add(moveVector);
-    }
-  }
-
-  //LOCAL STORAGE
-  localStorage.setItem('speed', runling.speed);
+    //LOCAL STORAGE
+    localStorage.setItem('speed', runling.speed);
 
 }
 
 function mousePressed() {
-  runlingMove = true;
-  runlingDestination.x = mouseX;
-  runlingDestination.y = mouseY;
+    runlingMove = true;
+    runlingDestination.x = mouseX;
+    runlingDestination.y = mouseY;
 }
 
 
 function keyPressed() {
-  if (keyCode == 83) {
-    runlingMove = false;
-  }
-  if (keyCode == UP_ARROW) {
-    runling.speed += 0.2;
-  } else if (keyCode == DOWN_ARROW) {
-    runling.speed -= 0.2;
-  }
-  if (keyCode == 71) {
-    godMode *= -1
-  }
+    if (keyCode == 83) {
+        runlingMove = false;
+    }
+    if (keyCode == UP_ARROW) {
+        runling.speed += 0.2;
+    } else if (keyCode == DOWN_ARROW) {
+        runling.speed -= 0.2;
+    }
+    if (keyCode == 71) {
+        godMode *= -1
+    }
 
 }
